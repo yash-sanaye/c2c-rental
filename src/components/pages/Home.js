6a49dc5209f9fa56773d96526825fb9e6c6abc6d
bajrang55 <bajaranglondhe59525@gmail.com>
2021-10-06 17:35:55 +0530

    trial_products

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,11 +4,13 @@ import Cards from '../Cards';
 import HeroSection from '../HeroSection';
 import Footer from '../Footer';
 
+
 function Home() {
   return (
     <>
       <HeroSection />
       <Cards />
+     
       <Footer />
     </>
   );
--- src/components/pages/Products.js
+++ src/components/pages/Products.js
@@ -1,6 +1,171 @@
-import React from 'react';
-import '../../App.css';
 
-export default function Products() {
-  return <h1 className='products'>PRODUCTS</h1>;
+import React from 'react';
+import '../pages/Product.css';
+import Card from './Card';
+
+
+function Product() {
+  return (
+    <div className="container">
+      <div className="row">
+        <Card
+          title="What is Lorem Ipsum?"
+          images="../images/batman.png"
+          old_price="9,999"
+          newPrice="9999"
+          rupess="$"
+          alt="batman"
+          exp_date="25-10-2020"
+        />
+        <Card
+          title="What is Lorem Ipsum?"
+          images="../images/blackpanter.png"
+          old_price="599"
+          newPrice="500"
+          rupess="&#x20B9;"
+          alt="blackpanter"
+          exp_date="25-10-2020"
+        />
+        <Card
+          title="What is Lorem Ipsum?"
+          images="../images/arthur.png"
+          old_price="7999"
+          newPrice="7000"
+          rupess="$"
+          alt="arthur"
+          exp_date="25-10-2020"
+        />
+        <Card
+          title="What is Lorem Ipsum?"
+          images="../images/kashima.png"
+          old_price="999"
+          newPrice="500"
+          rupess="$"
+          alt="kashima"
+          exp_date="25-10-2020"
+        />
+      </div>
+      <div className="container">
+      <div className="row">
+        <Card
+          title="What is Lorem Ipsum?"
+          images="../images/batman.png"
+          old_price="9,999"
+          newPrice="9999"
+          rupess="$"
+          alt="batman"
+          exp_date="25-10-2020"
+        />
+        <Card
+          title="What is Lorem Ipsum?"
+          images="../images/blackpanter.png"
+          old_price="599"
+          newPrice="500"
+          rupess="&#x20B9;"
+          alt="blackpanter"
+          exp_date="25-10-2020"
+        />
+        <Card
+          title="What is Lorem Ipsum?"
+          images="../images/arthur.png"
+          old_price="7999"
+          newPrice="7000"
+          rupess="$"
+          alt="arthur"
+          exp_date="25-10-2020"
+        />
+        <Card
+          title="What is Lorem Ipsum?"
+          images="../images/kashima.png"
+          old_price="999"
+          newPrice="500"
+          rupess="$"
+          alt="kashima"
+          exp_date="25-10-2020"
+        />
+      </div>
+      <div className="container">
+      <div className="row">
+        <Card
+          title="What is Lorem Ipsum?"
+          images="../images/batman.png"
+          old_price="9,999"
+          newPrice="9999"
+          rupess="$"
+          alt="batman"
+          exp_date="25-10-2020"
+        />
+        <Card
+          title="What is Lorem Ipsum?"
+          images="../images/blackpanter.png"
+          old_price="599"
+          newPrice="500"
+          rupess="&#x20B9;"
+          alt="blackpanter"
+          exp_date="25-10-2020"
+        />
+        <Card
+          title="What is Lorem Ipsum?"
+          images="../images/arthur.png"
+          old_price="7999"
+          newPrice="7000"
+          rupess="$"
+          alt="arthur"
+          exp_date="25-10-2020"
+        />
+        <Card
+          title="What is Lorem Ipsum?"
+          images="../images/kashima.png"
+          old_price="999"
+          newPrice="500"
+          rupess="$"
+          alt="kashima"
+          exp_date="25-10-2020"
+        />
+      </div>
+      <div className="container">
+      <div className="row">
+        <Card
+          title="What is Lorem Ipsum?"
+          images="../images/batman.png"
+          old_price="9,999"
+          newPrice="9999"
+          rupess="$"
+          alt="batman"
+          exp_date="25-10-2020"
+        />
+        <Card
+          title="What is Lorem Ipsum?"
+          images="../images/blackpanter.png"
+          old_price="599"
+          newPrice="500"
+          rupess="&#x20B9;"
+          alt="blackpanter"
+          exp_date="25-10-2020"
+        />
+        <Card
+          title="What is Lorem Ipsum?"
+          images="../images/arthur.png"
+          old_price="7999"
+          newPrice="7000"
+          rupess="$"
+          alt="arthur"
+          exp_date="25-10-2020"
+        />
+        <Card
+          title="What is Lorem Ipsum?"
+          images="../images/kashima.png"
+          old_price="999"
+          newPrice="500"
+          rupess="$"
+          alt="kashima"
+          exp_date="25-10-2020"
+        />
+      </div>
+    </div>
+    </div>
+    </div>
+    </div>
+  );
 }
+export default Product;
