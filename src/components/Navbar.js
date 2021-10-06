99b07efaae425df4401c53e6b275371f56fbda43
bajrang55 <bajaranglondhe59525@gmail.com>
2021-10-06 17:57:19 +0530

    demoproduct

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -52,7 +52,7 @@ function Navbar() {
             </li>
             <li className='nav-item'>
               <Link
-                to='/'
+                to='Products/'
                 className='nav-links'
                 onClick={closeMobileMenu}
               >
